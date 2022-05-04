420ed8b5998a59d603e7ef6c87d588a0134cbadf
Milton <milton.hneto@gmail.com>
2022-05-04 14:42:55 -0300

    Integração com API Github

--- src/ProfileShow/index.tsx
+++ src/ProfileShow/index.tsx
@@ -1,20 +1,26 @@
-import Avatar from '../assets/images/avatar.png';
-
 import './styles.css';
 
-const ProfileShow = () => {
+interface ProfileShowProps {
+  avatar_url: string,
+  url: string,
+  name: string,
+  location: string,
+  followers: number
+}
+
+const ProfileShow = ({avatar_url, url, name, location, followers} : ProfileShowProps) => {
   return (
     <div className="profile-show-container">
       <div className="avatar">
-        <img src={Avatar}  alt="Imagem do perfil" />
+        <img src={avatar_url}  alt="Imagem do perfil" />
       </div>
       <div className="infos">
         <h2>Informações</h2>
         <div className="infos-items">
-        <p><span>Perfil:</span> <a href="/">https://api.github.com/users/acenelio</a></p>
-        <p><span>Seguidores:</span> 4379</p>
-        <p><span>Localidade:</span> Uberlândia</p>
-        <p><span>Nome:</span> Nelio Alves</p>
+        <p><span>Perfil:</span> <a href="/">{url}</a></p>
+        <p><span>Seguidores:</span>{followers}</p>
+        <p><span>Localidade:</span>{location}</p>
+        <p><span>Nome:</span>{name}</p>
         </div>
       </div>
     </div>
--- src/pages/ProfileSearch/index.tsx
+++ src/pages/ProfileSearch/index.tsx
@@ -1,18 +1,71 @@
+import axios from 'axios';
 import ProfileShow from 'ProfileShow';
+import { useState } from 'react';
 import './styles.css';
 
+
+type FormData = {
+  username: string;
+}
+
+type Profile = {
+  avatar_url: string,
+  url: string,
+  name: string,
+  location: string,
+  followers: number
+}
+
 const ProfileSearch = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    username: '',
+  });
+
+  const [profile, setProfile] = useState<Profile>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({...formData, [name]: value});
+    }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios.get(`https://api.github.com/users/${formData.username}`)
+    .then((response) => {
+      setProfile(response.data);
+    }).catch((error) => {
+      setProfile(undefined);
+      console.log(error);
+    });
+  }
+
   return (
     <div className="container">
       <div className="profile-search-container">
         <h1>
           Encontre um perfil Github
         </h1>
-        <input type="text" value="Usuário Github" />
+        <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name="username"
+          value={formData.username}
+          placeholder="Usuário github"
+          onChange={handleChange}
+          />
         <button type="submit">Encontrar</button>
+        </form>
       </div>
       <div className="info-container">
-        <ProfileShow />
+        {profile &&
+  
+          <ProfileShow {...profile} />
+
+        }
       </div>
     </div>
   );
